db3485539790b501d6a901fff68afb648c6d431b
Jonathan Naylor <naylorjs@yahoo.com>
2017-06-11 08:02:15 +0100

    Bump the version date.

--- MMDVMCal.cpp
+++ MMDVMCal.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2015,2016 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2015,2016,2017 by Jonathan Naylor G4KLX
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -117,7 +117,7 @@ int CMMDVMCal::run()
 				break;
 			case 'V':
 			case 'v':
-				::fprintf(stdout, "MMDVMCal 20161228" EOL);
+				::fprintf(stdout, "MMDVMCal 20170610" EOL);
 				break;
 			case 'D':
 				setDMRDeviation();
