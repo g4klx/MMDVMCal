b6c6a4f99235e499e88e7ef8392ae23309955ffb
Jonathan Naylor <naylorjs@yahoo.com>
2020-11-24 16:16:10 +0000

    Add higher speeds for the serial controller.

--- SerialController.cpp
+++ SerialController.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2002-2004,2007-2011,2013,2014-2017 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2002-2004,2007-2011,2013,2014-2017,2020 by Jonathan Naylor G4KLX
  *   Copyright (C) 1999-2001 by Thomas Sailor HB9JNX
  *
  *   This program is free software; you can redistribute it and/or modify
@@ -338,6 +338,10 @@ bool CSerialController::open()
 			case SERIAL_230400:
 				::cfsetospeed(&termios, B230400);
 				::cfsetispeed(&termios, B230400);
+				break;
+			case SERIAL_460800:
+				::cfsetospeed(&termios, B460800);
+				::cfsetispeed(&termios, B460800);
 				break;
 			default:
 				::fprintf(stderr, "Unsupported serial port speed - %d" EOL, int(m_speed));
--- Version.h
+++ Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-#define VERSION "MMDVMCal 20200506"
+#define VERSION "MMDVMCal 20201124"
 
 #endif
