271ef4e68370770acf53f896e4ff365b5d73e958
Jonathan Naylor <naylorjs@yahoo.com>
2023-07-04 11:52:55 +0100

    Bump the version date.

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2018,2019,2020,2021 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2018,2019,2020,2021,2023 by Jonathan Naylor G4KLX
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-#define VERSION "MMDVMCal 20211101"
+#define VERSION "MMDVMCal 20230704"
 
 #endif
